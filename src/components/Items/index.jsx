399804d55cf4ac838f7ed14a5bd8e429a23de7d7
Lucas Cardoso <lucascardosopsd@gmail.com>
2022-04-08 18:44:05 -0400

    current location and current category verification

--- src/components/Items/index.jsx
+++ src/components/Items/index.jsx
@@ -5,56 +5,62 @@ import axios from 'axios'
 import { useLocation } from '../../context/Location'
 
 export default function Items({currentCategory}){
+  
+  const [items, setItems] = useState([])
 
-    const [items, setItems] = useState([])
+  const {currentLocation} = useLocation()
 
-    const {currentLocation} = useLocation()
+  // fetch current category items
+  useEffect(() => {
 
-    // fetch current category items
-    useEffect(() => {
+    async function fetchItems(){
 
-        const query = qs.stringify({
-        filters:{
-            osaka_jales_category: {
-            title:{
-                $eq: currentCategory
-            }
-            }
-        }
-        })
+      // const query = qs.stringify({
+      //   filters:{
+      //     osaka_jales_category: {
+      //       title:{
+      //         $eq: currentCategory
+      //       }
+      //     }
+      //   }
+      // })
 
-        axios.get(`${process.env.REACT_APP_SERVER_URL}/api/osaka-${currentLocation}-items?${query}`)
-        .then(({data}) => (
-        setItems(data.data)
-        ))
-        
-        // eslint-disable-next-line
-    }, [currentCategory])
+      const query = `filters[osaka_${currentLocation}_category][title][$eq]=${currentCategory}`
 
-    return(
-        <Container>
-        {
-          items &&
-          items.map(({attributes}, index) => (
-            <div key={index}>
-              
-              <div className='image'>
-                <img src={attributes.image_url} alt={"imagem de " + attributes.title}/>
-              </div>
+      let {data} = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api/osaka-${currentLocation}-items?${query}`)
+      setItems(data.data)
+    }
 
-              <div className='text'>
-                <h4>{attributes.title}</h4>
-                <p>{attributes.description}</p>
-                <h5>
-                  {new Intl.NumberFormat('pt-BR', 
-                  { style: 'currency', currency: 'BRL' })
-                  .format(attributes.price)}
-                </h5>
-                
-              </div>
+    currentCategory && fetchItems()
+
+    // eslint-disable-next-line
+  }, [currentCategory])
+
+  return(
+      <Container>
+      {
+        items &&
+        items.map(({attributes}, index) => (
+          <div key={index}>
+            <div className='image'>
+              <img src={attributes.image_url} alt={"imagem de " + attributes.title}/>
             </div>
-          ))
-        }
-      </Container>
-    )
+
+            <div className='text'>
+              <h4>{attributes.title}</h4>
+              <p>{attributes.description}</p>
+              <h5>
+                {new Intl.NumberFormat('pt-BR', 
+                { style: 'currency', currency: 'BRL' })
+                .format(attributes.price)}
+              </h5>
+              
+            </div>
+          </div>
+        ))
+
+
+      }
+    </Container>
+  )
 }
--- src/components/Items/styles.js
+++ src/components/Items/styles.js
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
 
+    margin: 0 auto;
+
     width: 100%;
     max-width: 720px;
     height: 100%;
